1a9713c4e152ad1205263f3998f40466ecae482b
Pablo Iago <evangelista.pabloiago@gmail.com>
2020-12-03 23:07:54 -0300

    logica 7 feita

--- bloco_4/dia_1/logica_dia1.js
+++ bloco_4/dia_1/logica_dia1.js
@@ -88,3 +88,24 @@ if(nome === "peão" || nome === "PEÃO"){
     console.log("ERRO")
 }
 */
+
+//Desafio 7
+/*
+let porcentagem = -2
+
+if(porcentagem > 100 || porcentagem < 0){
+    console.log("ERRO")
+}else if(porcentagem >= 90 && porcentagem <= 100){
+    console.log("A")
+}else if(porcentagem >= 80 && porcentagem < 90){
+    console.log("B")
+}else if(porcentagem >= 70 && porcentagem < 80){
+    console.log("C")
+}else if(porcentagem >= 60 && porcentagem < 70){
+    console.log("D")
+}else if(porcentagem >= 50 && porcentagem < 60){
+    console.log("E")
+}else{
+    console.log("F")
+}
+*/
