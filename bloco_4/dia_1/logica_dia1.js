f45f657a14fe537874d4e3f70ac040c733f48aca
Pablo Iago <evangelista.pabloiago@gmail.com>
2020-12-03 23:26:43 -0300

    logica 9 feita

--- bloco_4/dia_1/logica_dia1.js
+++ bloco_4/dia_1/logica_dia1.js
@@ -122,3 +122,16 @@ if(number1 % 2 === 0 || number2 % 2 === 0 || number3 % 2 === 0){
     console.log(false)
 }
 */
+
+//Desafio 9
+/*
+let number1 = 102
+let number2 = 10
+let number3 = 2
+
+if(number1 % 2 != 0 || number2 % 2 != 0 || number3 % 2 != 0){
+    console.log(true)
+}else{
+    console.log(false)
+}
+*/
