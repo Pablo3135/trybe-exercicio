3692d0408c266ce289ebf2200099801548996a97
Pablo Iago <evangelista.pabloiago@gmail.com>
2020-12-03 21:35:47 -0300

    logica 2 feita

--- bloco_4/dia_1/logica_dia1.js
+++ bloco_4/dia_1/logica_dia1.js
@@ -1,3 +1,4 @@
+/*
 let n1 = 7
 let n2 = 8
 let soma = n1 + n2
@@ -6,3 +7,14 @@ let mult = n1 * n2
 let divisão = n1 / n2
 let mód = n1 % n2
 console.log (soma, sub, mult, divisão, mód)
+*/
+/*
+let number1 = 1.5
+let number2 = 1
+
+if (number1 > number2){
+    console.log ("Number one é maior")
+}else{
+    console.log ("Number two é maior")
+}
+*/
