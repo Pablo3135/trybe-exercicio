5be303871babe7a2c11ad46d071bc58398108dad
Pablo Iago <evangelista.pabloiago@gmail.com>
2020-12-03 21:45:06 -0300

    logica 3 feita

--- bloco_4/dia_1/logica_dia1.js
+++ bloco_4/dia_1/logica_dia1.js
@@ -1,3 +1,5 @@
+//Desafio 1 
+
 /*
 let n1 = 7
 let n2 = 8
@@ -8,6 +10,9 @@ let divisão = n1 / n2
 let mód = n1 % n2
 console.log (soma, sub, mult, divisão, mód)
 */
+
+//Desafio 2
+
 /*
 let number1 = 1.5
 let number2 = 1
@@ -18,3 +23,17 @@ if (number1 > number2){
     console.log ("Number two é maior")
 }
 */
+
+//Desafio 3 
+
+let number1 = 4
+let number2 = 3
+let number3 = 5
+
+if (number1 > number2 && number1 > number3){
+    console.log("Number one e maior")
+}else if(number2 > number1 && number2 > number3){
+    console.log("Number two é maior")
+}else {
+    console.log("Number tree é maior")
+}
