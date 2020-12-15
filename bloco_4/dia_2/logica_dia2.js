c4b40245bc7f1543cc8bd9b3bc8bb005276261e6
Pablo Iago <evangelista.pabloiago@gmail.com>
2020-12-15 20:34:02 -0300

    exercicio 2

--- bloco_4/dia_2/logica_dia2.js
+++ bloco_4/dia_2/logica_dia2.js
@@ -1,3 +1,13 @@
 //exercicio 1
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-console.log(numbers)
+//console.log(numbers)
+
+//exercicio 2
+/*
+let total = 0
+for(i = 0; i < numbers.length; i++){
+total = total + numbers[i];
+console.log('O total é ' + total)
+}
+*/
+
