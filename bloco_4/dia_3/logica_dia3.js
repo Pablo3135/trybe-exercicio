cc13a86cb6d5a6009399e9d28605e51c22cb4d43
Pablo Iago <evangelista.pabloiago@gmail.com>
2020-12-16 13:19:44 -0300

    Exercicio 2

--- bloco_4/dia_3/logica_dia3.js
+++ bloco_4/dia_3/logica_dia3.js
@@ -11,4 +11,17 @@ for(i = 0; i < n ; i++){
 for(i = 0; i < n; i++){
     console.log(inputLine)
 }
+*/
 
+//exercicio 2
+/*
+let n = 5
+let i;
+let symbol = '*';
+let inputLine = '';
+
+for(i = 0; i < n ; i++){
+    inputLine = inputLine + symbol;
+    console.log(inputLine)
+}
+*/
