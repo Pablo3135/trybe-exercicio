4b6595d8e52f3c2d95fa27b1124e3b3d73479ebd
Pablo Iago <evangelista.pabloiago@gmail.com>
2020-12-21 16:38:36 -0300

    Requisito 2

--- bloco_4/dia_4/script.js
+++ bloco_4/dia_4/script.js
@@ -1,7 +1,15 @@
+
 let info = {
     personagem: "Margarida",
     origem: "Pato Donalds",
     nota: "Namorada do Personagem principal nos quadrinho do Pato Donalds",
+    recorrente: "Sim"
 };
 
+// exercicio 1
+/*
 console.log(`Bem-Vindo ${info.personagem}`)
+*/
+
+// exercicio 2
+/*console.log(info)*/
