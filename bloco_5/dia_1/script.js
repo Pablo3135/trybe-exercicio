3bb2fb29d6163c1453bbdd0aba304065b5ac5565
Pablo Iago <evangelista.pabloiago@gmail.com>
2020-12-28 16:28:21 -0300

    add estilizaçao pelo js

---
+++ bloco_5/dia_1/script.js
@@ -0,0 +1,8 @@
+let meta = document.getElementById("meta")
+console.log(meta.innerText = "Eu me vejo daqui a dois anos formado na Trybe e com uma boa remuneração para assim poder pagalos");
+
+let cor = document.getElementsByClassName("main-content")[0]
+cor.style.background = "rgb(76,164,109)"
+
+let cor2 = document.getElementsByClassName("center-content")[0]
+cor2.style.background = "White"
